d6d515f6b5df63e9d88dedf104eb1c38425c273e
Steve Abbott <ttsabbott@gmail.com>
2024-11-27 21:32:24 -0700

    updated with router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+
+// pages
+import Home from './pages/Home.jsx';
+import About from './pages/About.jsx';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -17,7 +22,21 @@ function App() {
         </a>
       </div>
       <h1>React Router</h1>
-      <h5>Have fun!</h5>
+      <BrowserRouter>
+        <header>
+          <nav>
+            {/* <h1>Jobarouter</h1> */}
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="about">About</NavLink>
+          </nav>
+        </header>
+        <main>
+          <Routes>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+          </Routes>
+        </main>
+      </BrowserRouter>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
