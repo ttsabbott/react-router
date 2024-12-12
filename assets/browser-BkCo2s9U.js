b5fa602112a19b559e383a958c0c9bf902eda3a7
ttsabbott <ttsabbott@users.noreply.github.com>
2024-12-12 04:31:33 +0000

    deploy: 178c293aaf52117e625c9fc5967068b3df7fa0fe

---
+++ assets/browser-BkCo2s9U.js
@@ -0,0 +1,1 @@
+import{g as o}from"./index-Wn4vy8Bu.js";var r,e;function t(){return e||(e=1,r=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}),r}var s=t();const n=o(s),w=Object.freeze(Object.defineProperty({__proto__:null,default:n},Symbol.toStringTag,{value:"Module"}));export{w as b};
